8cd0e35a1b9d382e35c4988cdd227416fa22b7f1
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-05-05 18:23:29 +0400

    finished version of app

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './index.css';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
+
+function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [invites, setInvites] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+
+
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data);
+    }).catch(err => {
+      console.warn(err);
+      alert('Ошибка при получении пользователей')
+    }).finally(() => setLoading(false));
+  }, []);
+
+  // input search bar 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+  // invitation btn svg
+  const onClickInvite = (id) => {
+    if(invites.includes(id)){
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  }
+  // success btn
+  const onCLickSendInvites = () => {
+    setSuccess(true)
+  }
+
+
+  return (
+    <div className="App">
+      {
+        success ? <Success count={invites.length} /> :
+        <Users 
+      items={users} 
+      isLoading={isLoading} 
+      searchValue={searchValue}
+      onChangeSearchValue={onChangeSearchValue}
+      invites={invites}
+      onClickInvite={onClickInvite}
+      onCLickSendInvites={onCLickSendInvites}
+      />
+      }
+      
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Success.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Success = ({ count }) => {
+  return (
+    <div class="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем {count} пользователям отправлено приглашение.</p>
+      <a href="/">
+      <button className="send-invite-btn">Назад</button>
+      </a>
+      
+    </div>
+  );
+};
